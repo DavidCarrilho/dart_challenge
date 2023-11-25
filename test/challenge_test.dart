6fc2ab8459ce4868449e67cb27ad02b602e4dd13
DavidCarrilho <luan.david.oliver@gmail.com>
2023-11-25 00:45:14 -0300

    chore: create tests

--- test/challenge_test.dart
+++ test/challenge_test.dart
@@ -1,1 +1,12 @@
-void main() {}
+import 'package:challenge/challenge.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Teste informando o número 10', () {
+    expect(getNumbersOfSum(10), equals('3 + 5 + 6 + 9 = 23'));
+  });
+
+  test('Teste informando o número 11', () {
+    expect(getNumbersOfSum(11), equals('3 + 5 + 6 + 9 + 10 = 33'));
+  });
+}
